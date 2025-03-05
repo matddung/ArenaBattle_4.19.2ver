3508c71f0f925ee606556a94c14d9e2e3220760f
matddung <matddung1111@gmail.com>
2025-03-06 03:56:29 +0900

    first commit

---
+++ Source/ArenaBattle/Fountain.cpp
@@ -0,0 +1,20 @@
+#include "Fountain.h"
+
+AFountain::AFountain()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AFountain::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AFountain::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
